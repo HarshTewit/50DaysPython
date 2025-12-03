a3c6a5c306d334836ea7231cecfb0bfe472847b9
HarshTewit <harshitutewari@gmail.com>
2025-12-03 15:40:20 +0530

    Data Sc

---
+++ Data Science /salary_pre.py
@@ -0,0 +1,32 @@
+import pandas as pd 
+import matplotlib.pyplot as ax 
+from sklearn.linear_model import LinearRegression 
+import streamlit as st 
+#Load data set 
+data = pd.read_csv("Experience_salary.csv")
+
+X = data[["YearsExperience"]]
+y = data[["Salary"]]
+model = LinearRegression()
+model.fit(X, y)
+st.title("Salary Predictor Based on Experience")
+st.write("Enter your years of experience to predict your salary: ")
+
+years_input = st.number_input("Years of experience", min_value = 0.0, max_value= 50.0, step=0.1)
+
+if years_input:
+    print(years_input)
+
+    predicted_salary = model.predict([[years_input]])[0][0]
+    st.success(f"Estimated Salary {predicted_salary}")
+
+st.subheader("Regression Line")
+
+fig, ax = ax.subplots()
+ax.scatter(X, y, color="blue", label="Actual Data")
+ax.plot(X, model.predict(X), color="red", label="Regression line")
+ax.set_xlabel("Years of Experience")
+ax.set_ylabel("Salary")
+ax.set_title("Salary vs Experience")
+ax.legend()
+st.pyplot(fig)
